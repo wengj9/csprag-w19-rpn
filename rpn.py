8cac67766e5f83d416a26a7e05b83b3ca5d62a4e
James Weng <wengj@umich.edu>
2019-03-02 18:54:43 -0500

    Add support for more math functions
    Add support for factorial and basic trigonometric functions

--- rpn.py
+++ rpn.py
@@ -2,6 +2,7 @@
 
 import operator
 import readline
+import math
 
 
 operators = {
@@ -11,24 +12,43 @@ operators = {
     '/': operator.truediv,
     '^': operator.pow,
     '%': operator.mod,
+    '!': math.factorial,
+    'sin': math.sin,
+    'cos': math.cos,
+    'tan': math.tan,
+}
+
+one_operators = ['!', 'sin', 'cos', 'tan']
+
+constants = {
+    'pi': math.pi,
+    'e': math.e,
 }
 
 def calculate(myarg):
-    stack = list()
-    for token in myarg.split():
-        try:
-            token = int(token)
-            stack.append(token)
-        except ValueError:
-            function = operators[token]
-            arg2 = stack.pop()
-            arg1 = stack.pop()
-            result = function(arg1, arg2)
-            stack.append(result)
-        print(stack)
-    if len(stack) != 1:
-        raise TypeError("Too many parameters")
-    return stack.pop()
+        stack = list()
+        for token in myarg.split():
+            try:
+                token = int(token)
+                stack.append(token)
+            except ValueError:
+                if (token in constants):
+                    token = constants[token]
+                    stack.append(token)
+                elif (token in one_operators):
+                    function = operators[token]
+                    arg1 = stack.pop()
+                    result = function(arg1)
+                    stack.append(result)
+                else:
+                    function = operators[token]
+                    arg2 = stack.pop()
+                    arg1 = stack.pop()
+                    result = function(arg1, arg2)
+                    stack.append(result)
+        if len(stack) != 1:
+            raise TypeError("Too many parameters")
+        return stack.pop()
 
 def main():
     while True:
--- test_rpn.py
+++ test_rpn.py
@@ -18,3 +18,18 @@ class TestBasics(unittest.TestCase):
     def test_exponent(self):
         result = rpn.calculate("2 3 ^")
         self.assertEqual(8,result)
+    def test_modulus(self):
+        result = rpn.calculate("13 3 %")
+        self.assertEqual(1,result)
+    def test_factorial(self):
+        result = rpn.calculate("4 !")
+        self.assertEqual(24,result)
+    def test_sin(self):
+        result = rpn.calculate("pi sin")
+        self.assertAlmostEqual(0,result)
+    def test_cos(self):
+        result = rpn.calculate("pi cos")
+        self.assertEqual(-1,result)
+    def test_tan(self):
+        result = rpn.calculate("pi tan")
+        self.assertAlmostEqual(0,result)
